7c7da3075f7dfa12d760b539280f311e40216b37
Kristen Edwards <k.leigh.edwards@gmail.com>
2016-07-23 12:03:19 -0600

    Functioning Program Will Provide Accurate Change

--- src/change/CashRegister.java
+++ src/change/CashRegister.java
@@ -9,25 +9,91 @@ public class CashRegister
 		
 		System.out.print("List the price of the item: ");
 		price = keyboard.nextDouble();
+		price = price * 100;
 		
-		System.out.print("How much money did the customer give you: ");
+		System.out.print("The customer provided: ");
 		tendered = keyboard.nextDouble();
+		tendered = tendered * 100;
+		
+		keyboard.close();
+		
 		
 		if (tendered < price)
 		{
-			System.out.println("ERROR: Insuficient Funds");
+			System.out.println("\nERROR: Insufficient Funds");
 		}
 		
 		else if (tendered == price)
 		{
-			System.out.println("The customer has given you exact change.");
+			System.out.println("\nThe customer has given you exact change.");
 			System.out.println("No further action required.");
 		}
 		
 		else
 		{
-			System.out.println("The customer has given you too much money.");
-			System.out.println("You must give them their change.");
+			System.out.println("\nThe customer has given you too much money.");
+			System.out.println("Their change:\n");
+			
+			int change = (int)(tendered - price);
+			
+			if (change >= 2000)
+			{
+				int twentyDollar = (int)(change / 2000);
+				System.out.println("Twenty Dollar Bills: " + twentyDollar);
+				
+				change = change % 2000;
+			}
+			if (change >= 1000)
+			{
+				int tenDollar = (int)(change / 1000);
+				System.out.println("Ten Dollar Bills: " + tenDollar);
+				
+				change = change % 1000;
+			}
+			if (change >= 500)
+			{
+				int fiveDollar = (int)(change / 500);
+				System.out.println("Five Dollar Bills: " + fiveDollar);
+				
+				change = change % 500;
+			}
+			if (change >= 100)
+			{
+				int oneDollar = (int)(change / 100);
+				System.out.println("One Dollar Bills: " + oneDollar);
+				
+				change = change % 100;
+			}
+			if (change >= 25)
+			{
+				int quarter = (int)(change / 25);
+				System.out.println("Quarters: " + quarter);
+				
+				change = change % 25;
+			}
+			if (change >= 10)
+			{
+				int dime = (int)(change / 10);
+				System.out.println("Dimes: " + dime);
+				
+				change = change % 10;
+			}
+			if (change >= 5)
+			{
+				int nickel = (int)(change / 5);
+				System.out.println("Nickels: " + nickel);
+				
+				change = change % 5;
+			}
+			if (change < 5)
+			{
+				int penny = (int)(change);
+				if (penny > 0)
+				{
+					System.out.println("Pennies: " + penny);
+				}
+
+			}
 		}
 	}
 }
